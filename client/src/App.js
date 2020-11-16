5040abc5f4b6ea90df375db88f991f4657b73e7d
Euphory <osoriomando2345@gmail.com>
2020-11-15 21:12:35 -0800

    for pull

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,15 @@ import axios from "axios";
 import BookList from "./components/BookList";
 import { Container, Row, Col } from "./components/Grid";
 import Books from "./pages/MyBooks";
+//import { proxy } from "../package.json";
+
+
+//module.exports = router;
 
 function App() {
   const [book, setBook] = useState("");
   const [result, setResult] = useState([]);
-  const [apiKey, setApiKey] = useState("AIzaSyBXh-q5PwWPI2nNsQEaJr4Zloljeza2VGc"); // Add API Key Here !!
+  const [apiKey] = useState("AIzaSyBXh-q5PwWPI2nNsQEaJr4Zloljeza2VGc"); // Add API Key Here !!
 
   const handleInputChange = event => {
     // Destructure the name and value properties off of event.target
@@ -45,15 +49,45 @@ function App() {
     var description = event.target.parentElement.querySelector("p").innerHTML;
     var image = event.target.parentElement.parentElement.querySelector("img").src;
     var link = event.target.parentElement.querySelector("a").href;
-    // console.log("being clicked", event.target.parentElement.querySelector("h3").innerHTML);
-    // console.log("being clicked", event.target.parentElement.querySelector("h4").innerHTML);
-    // console.log("being clicked", event.target.parentElement.querySelector("p").innerHTML);
-    // console.log("being clicked", event.target.parentElement.parentElement.querySelector("img").src);
+    
     console.log("TITLE", title);
     console.log("AUTHORS", authors);
     console.log("DESCRIPTION", description);
     console.log("IMAGE", image);
     console.log("LINK", link);
+
+    let book = {
+      title: title,
+      authors: authors,
+      description: description,
+      image: image,
+      link: link
+    };
+
+    console.log("Book object", book);
+
+    //axios.post("/books", { book });
+
+    axios.post("/books", { book })
+    .then(response => {    
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+       
+      }
+      else {
+        // clear form
+       
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+    
+  
+      // clear form
+      
+    });
   }
 
   return (
--- client/src/pages/MyBooks.js
+++ client/src/pages/MyBooks.js
@@ -1,10 +1,60 @@
 import React from "react";
+import BookList from "../components/BookList";
+import { Container, Row, Col } from "../components/Grid";
+//import { proxy } from "../../package.json";
 
 function MyBooks() {
+    let books = [];
+    fetch(`/books`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // save db data on global variable
+      books = data;
+    });
     return(
-        <>
-        </>
-    );
+    //   books.forEach(book => {
+    //     // create and populate a table row
+    //     return(
+    //     <>
+    //     <h3>{book.volumeInfo.title}</h3>
+    //     <h4>Author(s): {book.volumeInfo.authors}</h4>
+    //     <p>Description: {book.volumeInfo.description}</p>
+    //     <a rel="noreferrer noopener" target="_blank" href={book.volumeInfo.infoLink}>
+    //       Go to book!
+    //     </a>
+    //     </>
+    //     )
+    //   })
+
+    <BookList>
+    {books.map(book => {
+      return (
+        <li key={book.volumeInfo.title} className="list-group-item">
+          <Container>
+            <Row>
+              <Col size="xs-4 sm-2">
+                <img
+                  src={book.volumeInfo.imageLinks.thumbnail}
+                  alt={book.volumeInfo.title}
+                />
+              </Col>
+              <Col size="xs-8 sm-9">
+                <h3>{book.volumeInfo.title}</h3>
+                <h4>Author(s): {book.volumeInfo.authors}</h4>
+                <p>Description: {book.volumeInfo.description}</p>
+                <a rel="noreferrer noopener" target="_blank" href={book.volumeInfo.infoLink}>
+                  Go to book!
+                </a>
+              </Col>
+            </Row>
+          </Container>
+        </li>
+      );
+    })}
+  </BookList>
+  );
 }
 
 export default MyBooks;
